dc915f9b595169f45a5982def67ea15a92b7ab0e
Ari Durlacher <arikodcode@gmail.com>
2025-12-11 10:00:47 +0200

    create searchStock function

--- utils/searchStock.js
+++ utils/searchStock.js
@@ -1,1 +1,15 @@
-export default function searchStock(identifier) {}
+import { stocks } from '../stocks/stocks.js';
+export default function searchStock(identifier) {
+  const list = stocks;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i]['name'] === identifier) {
+      return list[i];
+    }
+  }
+  for (let i = 0; i < list.length; i++) {
+    if (list[i]['id'] === identifier) {
+      return list[i];
+    }
+  }
+  console.log('stock not found');
+}
